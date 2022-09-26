6fb49437a7814d0b714f22f68bd87cf7363461ec
Virginie Morque <v.morque@capitechnic.com>
2022-09-26 12:00:55 +0200

    projet 4

---
+++ projet_4/app.js
@@ -0,0 +1,10 @@
+const inputs = Array.from(document.querySelectorAll('.controls input'))
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+
+function handleUpdate() {
+
+    const suffix = this.dataset.sizing || ''
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+
+}
